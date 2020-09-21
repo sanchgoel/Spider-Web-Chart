ceeeb50b363eddd17b0e1a90761ecde712fab164
Sanchit Goel <sanch.goel@gmail.com>
2020-09-21 18:11:05 +0530

    [#Develop] Graph values and labels added

--- Spider Web Chart/SpiderWebChartView.swift
+++ Spider Web Chart/SpiderWebChartView.swift
@@ -11,20 +11,23 @@ import UIKit
 
 class SpiderWebChartView: UIView {
       
-  var bgWebColor = UIColor(hex: "#DFE6F1")
-  var parameterLineStrokeColor = UIColor(hex: "#B5BFCF")
-  var numberOfParameters = 8
+  var bgWebColor = UIColor(hex: "#5E6B7F")?.withAlphaComponent(0.8)
+  var parameterLineStrokeColor = UIColor(hex: "#707781")?.withAlphaComponent(0.8)
+  var parameters = [String]()
+  var parameterValues = [Int]()
   
   override func draw(_ rect: CGRect) {
     createWebBackground(rect: rect)
     addParamterLines(rect: rect)
+    plotValues(rect: rect)
+    addParameterLabels(rect: rect)
   }
   
   func addParamterLines(rect: CGRect) {
-    let radius = rect.width*0.75/2
+    let radius = rect.width/2
     let center = CGPoint(x: rect.midX, y: rect.midY)
-    for count in 0..<numberOfParameters {
-      let angularDifference = 360.0/Double(numberOfParameters)
+    for count in 0..<parameterValues.count {
+      let angularDifference = 360.0/Double(parameterValues.count)
   
       let path = UIBezierPath()
       path.move(to: center)
@@ -46,7 +49,7 @@ class SpiderWebChartView: UIView {
   
   func createWebBackground(rect: CGRect) {
     // Set the center and radius of polgons according to the parent view
-    let radius = rect.width*0.75/2
+    let radius = rect.width/2
     let center = CGPoint(x: rect.midX, y: rect.midY)
     drawConcentricBgPolygons(radius: radius, center: center)
   }
@@ -70,9 +73,9 @@ class SpiderWebChartView: UIView {
                              y: center.y + radius*sin(0))
     path.move(to: firstPoint)
         
-    for count in 1..<numberOfParameters {
+    for count in 1..<parameterValues.count {
       // Find the difference of angles between two parameters
-      let angularDifference = 360.0/Double(numberOfParameters)
+      let angularDifference = 360.0/Double(parameterValues.count)
       
       // Find the next point of polygon
       let nextAngle = angularDifference * Double(count) * .pi / 180.0
@@ -95,5 +98,123 @@ class SpiderWebChartView: UIView {
     
     // Add the shapelayer to the view
     self.layer.addSublayer(shape)
+  }
+  
+  func plotValues(rect: CGRect) {
+    let path = UIBezierPath()
+    
+    let radius = rect.width/2
+    let center = CGPoint(x: rect.midX, y: rect.midY)
+    
+    path.lineWidth = 1.0
+    UIColor.clear.setStroke()
+        
+    let currentParameterValue = CGFloat(parameterValues[0])/100.0
+    let deltaX = radius*cos(0)*currentParameterValue
+    let deltaY = radius*sin(0)*currentParameterValue
+    
+    let firstPoint = CGPoint(x: center.x + deltaX,
+                             y: center.y + deltaY)
+    
+    path.move(to: firstPoint)
+    
+    for count in 1..<parameterValues.count {
+      let angularDifference = 360.0/Double(parameterValues.count)
+      
+      let nextAngle = angularDifference * Double(count) * .pi / 180.0
+      
+      let currentParameterValue = CGFloat(parameterValues[count])/100
+      let nextDeltaX = radius*CGFloat(cos(nextAngle))*currentParameterValue
+      let nextDeltaY = radius*CGFloat(sin(nextAngle))*currentParameterValue
+      
+      let nextPoint = CGPoint(x: center.x + nextDeltaX,
+                              y: center.y + nextDeltaY)
+      path.addLine(to: nextPoint)
+    }
+    path.close()
+    
+    path.addClip()
+    addGradient()
+  }
+  
+  func addGradient() {
+    // create and add the gradient
+    let colors = [UIColor(red: 48.0/255.0, green: 155.0/255.0, blue: 255.0/255.0, alpha: 0.8).cgColor,
+                  UIColor(red: 152.0/255.0, green: 72.0/255.0, blue: 255.0/255.0, alpha: 0.8).cgColor] as CFArray
+        
+    let colorSpace = CGColorSpaceCreateDeviceRGB()
+    
+    let colorLocations:[CGFloat] = [0.0, 1.0]
+        
+    guard let gradient = CGGradient(colorsSpace: colorSpace,
+                                    colors: colors,
+                                    locations: colorLocations) else { return }
+    
+    guard let context = UIGraphicsGetCurrentContext() else { return }
+    let startPoint = CGPoint(x: 1, y: 1)
+    let endPoint = CGPoint(x: 1, y: bounds.maxY)
+    
+    // and lastly, draw the gradient.
+    context.drawLinearGradient(gradient,
+                               start: startPoint,
+                               end: endPoint,
+                               options: CGGradientDrawingOptions.drawsAfterEndLocation)
+  }
+  
+  private func addParameterLabels(rect: CGRect) {
+    let radius = rect.width/2
+    let angularDifference = 360.0/Double(parameterValues.count)
+    
+    for count in 0..<parameters.count {
+      // Find the next point of polygon
+      let nextAngle = angularDifference * Double(count) * .pi / 180.0
+      let xDisp = radius*CGFloat(cos(nextAngle))
+      let yDisp = radius*CGFloat(sin(nextAngle))
+      addStackView(xDisp: xDisp,
+                   yDisp: yDisp,
+                   parameterName: parameters[count],
+                   parameterValue: parameterValues[count])
+    }
+  }
+  
+  func addStackView(xDisp: CGFloat,
+                    yDisp: CGFloat,
+                    parameterName: String,
+                    parameterValue: Int) {
+    
+    // Heading Label
+    let parameterLabel = UILabel()
+    parameterLabel.font = UIFont(name: "HelveticaNeue-Medium", size: 12.0)
+    parameterLabel.textColor = UIColor.black
+    parameterLabel.heightAnchor.constraint(equalToConstant: 16.0).isActive = true
+    parameterLabel.widthAnchor.constraint(equalToConstant: 100.0).isActive = true
+    parameterLabel.text = parameterName
+    parameterLabel.textAlignment = .center
+
+    // Value Label
+    let parameterValueLabel = UILabel()
+    parameterValueLabel.textColor = UIColor.black
+    parameterValueLabel.font = UIFont(name: "HelveticaNeue-Medium", size: 12.0)
+    parameterValueLabel.widthAnchor.constraint(equalToConstant: 100.0).isActive = true
+    parameterValueLabel.heightAnchor.constraint(equalToConstant: 16.0).isActive = true
+    parameterValueLabel.text = "\(parameterValue)%"
+    parameterValueLabel.textAlignment = .center
+
+    // Stack View
+    let stackView = UIStackView()
+    stackView.axis = NSLayoutConstraint.Axis.vertical
+    stackView.distribution = UIStackView.Distribution.equalSpacing
+    stackView.alignment = UIStackView.Alignment.center
+    stackView.spacing = 0.0
+
+    stackView.addArrangedSubview(parameterLabel)
+    stackView.addArrangedSubview(parameterValueLabel)
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+
+    self.addSubview(stackView)
+
+    // Constraints
+    stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: xDisp*1.22).isActive = true
+    stackView.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: yDisp*1.22).isActive = true
   }
 }
--- Spider Web Chart/ViewController.swift
+++ Spider Web Chart/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    let view = SpiderWebChartView(frame: CGRect(x: 62.5, y: 200, width: 250, height: 250))
+    view.parameters = ["Creativity", "Curiosity", "Eye For Detail", "Enthusiasm", "Patience", "Risk Taking", "Resourcefulness", "Perseverance"]
+    view.parameterValues = [90, 78, 88, 75, 84, 65, 83, 95]
+    view.backgroundColor = UIColor.clear
+    self.view.addSubview(view)
   }
 
 }
