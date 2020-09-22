d94d48d3bff953f8463338c0f9795b2306ad8c7e
Sanchit Goel <sanch.goel@gmail.com>
2020-09-22 12:05:13 +0530

    [#Develop] Image size corrected

--- Spider Web Chart/ViewController.swift
+++ Spider Web Chart/ViewController.swift
@@ -21,34 +21,34 @@ class ViewController: UIViewController {
                                                      y: 200,
                                                      width: 350,
                                                      height: 350))
-    
+
     // Add the text for parameters
     graphView.parameters = ["Creativity", "Curiosity", "Eye For Detail", "Enthusiasm",
                             "Patience", "Risk Taking"]
-    
+
     // Add the parameter values 0.0 - 1.0 for scale 1, 0.0 - 100.0 for scale 100
     graphView.parameterValues = [90, 78, 88, 95, 89, 83]
     graphView.scale = 100
-    
+
     // Add trailing text to parameter values
     graphView.parameterValueTrailingText = "%"
-    
+
     graphView.gradientColors = [UIColor(red: 210.0/255.0, green: 45.0/255.0, blue: 45.0/255.0, alpha: 1.0),
                                 UIColor(red: 221.0/255.0, green: 120.0/255.0, blue: 44.0/255.0, alpha: 1.0)]
-    
+
     // Set background color of the view
     graphView.backgroundColor = UIColor.clear
-    
+
     // Set Font Type and Color
     graphView.parameterFont = UIFont(name: "HelveticaNeue-Medium", size: 12.0)
     graphView.parameterFontColor = UIColor.black
-    
+
     // Set Distance Factor of Labels from center
     graphView.distanceOfLabelsFromCenter = 1.3
-    
+
     // Set the color of bg Web lines
     graphView.bgWebColor = UIColor(red: 94.0/255.0, green: 107.0/255.0, blue: 127.0/255.0, alpha: 0.8)
-    
+
     // Add the graph to the view
     self.view.addSubview(graphView)
   }
